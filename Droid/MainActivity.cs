aa626b3512c70d9c9598a484c851751f9da1f762
Nitin Pakhide <nishjeki@gmail.com>
2016-12-23 19:32:15 +0530

    Converting Xamarin.Forms to native Xamarin.Android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,19 +7,52 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
 
 namespace ShowPat.Forms.Droid
 {
 	[Activity (Label = "Forms.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	public class MainActivity : FragmentActivity
 	{
+		TabLayout tabLayout;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
+			SetContentView(Resource.Layout.Main);
 
-			LoadApplication (new App ());
+			tabLayout = FindViewById<TabLayout>(Resource.Id.sliding_tabs);
+
+			InitTabLayout();
+		}
+
+		void InitTabLayout()
+		{
+			tabLayout.SetTabTextColors(Android.Graphics.Color.Aqua, Android.Graphics.Color.AntiqueWhite);
+			//Fragment array
+			var fragments = new Android.Support.V4.App.Fragment[]
+			{
+				new YouTubeFragment(),
+				new DailyMotionFragment(),
+				new VimeoFragment(),
+			};
+
+			//Tab title array
+			var titles = CharSequence.ArrayFromStringArray(new[] {
+				"YouTube",
+				"DailyMotion",
+				"Vimeo",
+			});
+
+			var viewPager = FindViewById<ViewPager>(Resource.Id.viewpager);
+			//viewpager holding fragment array and tab title text
+			viewPager.Adapter = new TabsFragmentPagerAdapter(SupportFragmentManager, fragments, titles);
+
+			// Give the TabLayout the ViewPager 
+			tabLayout.SetupWithViewPager(viewPager);
 		}
 	}
 }
